70e35879daddaaa3453fe270a8cf172007c450e7
Marilia Vilar Pinto Ribeiro <mariliavilarpr@gmail.com>
2021-10-13 01:58:02 -0300

    feature: input text para nome do usuário, ao confirmar no botão o nome deve aparecer como título da página, acima

--- App.js
+++ App.js
@@ -1,26 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
 
-const hellos = ["Hello World", "Alô Mundo", "Hola Mundo", "Bonjour le monde"];
-const language = ["English", "Portuguese", "Spanish", "French", "Reiniciar"];
 
 const imageUriBackground = { uri: "https://cdn.wallpapersafari.com/57/1/e7y3Lh.jpg" };
 const imageUriProfile = { uri: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"}
 
 export default function App() {
-  const [index, setIndex] = useState(0);
-  const handlePress = () => (
-    setIndex((index+1) % language.length)  
+
+  const changeTitle = () => (
+    setTitle(userName)
   );
+
+  const [userName, onChangeUserName] = React.useState(null);
+  const [title, setTitle] = React.useState(null);
+
   return (
     <View style={styles.container}>
       <ImageBackground source={imageUriBackground} resizeMode="cover" style={styles.imageBackground}>
+
+        <Text style={styles.title}>{title}</Text>
+
         <View style={[styles.imageProfileContainer, { borderColor: 'white', borderWidth:2 }]}>
           <Image source={imageUriProfile} style={styles.imageProfile}/>
         </View>
-        <Text style={styles.text}>{hellos[index-1]}</Text>
-        <Button stryle={styles.buttons} title={language[index]} onPress={handlePress}/>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeUserName}
+          value={userName}
+          placeholder="digite para alterar o seu nome"
+        />
+
+        <View style={styles.buttons}>
+          <Button style={styles.buttons} title="Salvar" onPress={changeTitle} color="purple" accessibilityLabel="salvar nome do usuário"/>
+        </View>
+
         <StatusBar style="auto" />
       </ImageBackground>
     </View>
@@ -30,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   
   buttons: {
+    marginHorizontal: 120
   },
 
   container: {
@@ -42,6 +58,7 @@ const styles = StyleSheet.create({
   },
 
   imageProfileContainer: {
+    margin: 12,
     height: 120,
     width: 120,
     borderRadius: 120/2,
@@ -55,9 +72,17 @@ const styles = StyleSheet.create({
     borderRadius: 120/2,
   },
 
-  text: {
-    color: 'white',
-    alignSelf: 'center'
+  input: {
+    fontSize: 16,
+    margin: 20,
+    color: "white",
+    alignSelf: "center"
+  },
+
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    alignSelf: "center"
   }
 
 });
