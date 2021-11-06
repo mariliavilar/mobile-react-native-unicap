46887336dd09d1a2d4cbc5639cb1007f522fa710
Marilia Vilar Pinto Ribeiro <mariliavilarpr@gmail.com>
2021-11-06 17:05:47 -0300

    feature: adição dos steps necessários nos comentarios; add extras na navegação

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-//step01 - 'react-query' - [App.js] - Importar QueryClient e QueryClientProvider
+//step01 - 'react-navigation' - [App.js]
+//  - Importar NavigationContainer e createNativeStackNavigator
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HoroscopeScreen } from './screens/HoroscopeScreen';
 import { HomeScreen } from './screens/HomeScreen';
 
+//step02 - 'react-navigation' - [App.js]
+//  - Criar uma StackNavigator -> createNativeStackNavigator();
 const Stack = createNativeStackNavigator();
-//step02 - 'react-query' - [App.js] - Criar um client -> new QueryClient()
 const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* step03 - 'react-navigation' - [App.js]
+          - Incluir <NavigationContainer> e <Stack.Navigator> 
+          - e colocar as telas para navegação dentro delas */}
       <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator initialRouteName="Horoscope">
+          {/* /* step03 - 'react-navigation' - [App.js]
+            - Como especificar uma rota? usando o componente Screen
+            que recebe como propriedades name e component
+            - Aqui, usei a navegação por pilha (Stack) */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Horoscope" component={HoroscopeScreen} options={{ title: 'My Horoscope' }} />
+          {/* step04 - 'react-navigation' - [App.js]
+              - Extras:
+                  - definir initialRouteName no 'Navigator'
+                  - add prop 'options' no componente 'Screen' -> para opções específicas para certa(s) tela 
+                  - add prop 'screenOptions' no componente 'Navigator' -> para opções gerais em todas telas.*/}
         </Stack.Navigator>
-        </NavigationContainer>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 }
