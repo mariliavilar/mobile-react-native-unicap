ddaf62d282baefdb193ed520997354903364437c
Marilia Vilar Pinto Ribeiro <mariliavilarpr@gmail.com>
2021-11-28 13:11:29 -0300

    feature: personalização de cabeçalho de 3 formas distintas: 1- diretamente em uma tela especifica; 2- globalmente para todas as telas da pilha; 3- customizando um componente por completo para colocar no lugar do header

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import { View, Image, ImageBackground, Text } from 'react-native';
 import { QueryClient, QueryClientProvider } from 'react-query';
-//step01 - 'react-navigation' - [App.js]
-//  - Importar NavigationContainer e createNativeStackNavigator
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -10,32 +9,100 @@ import { HomeScreen } from './screens/HomeScreen';
 import { ProfileScreen } from './screens/ProfileScreen';
 import { MenuScreen } from './screens/MenuScreen';
 
-//step02 - 'react-navigation' - [App.js]
-//  - Criar uma StackNavigator -> createNativeStackNavigator();
 const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient()
+
+const imageHeader = { uri: "https://purewows3.imgix.net/images/articles/2018_06/Main-Horoscope-Header_Web.gif?auto=format,compress&cs=strip" };
+
+
+{/* Header com customização: */ }
+{/* MODE 3A - 'header-customization - [App.js] */ }
+{/* Header pode ser substituido por um componente completamente customizado, que pode conter texto, imagens, incluido gifs (android)​ */ }
+{/* -> renderizar numa funcao fora, e chamar depois*/ }
+function MyCustomHeaderView() {
+  return (
+    <View style={{ alignItems: 'center', flexDirection: "row", justifyContent: 'space-around' }}>
+      <Text style={{ color: 'white', marginRight: 10}}>
+          ***
+      </Text>
+      <ImageBackground
+        style={{ width: 200, height: 56 }}
+        source={imageHeader}
+      />
+      <Text style={{ color: 'white', marginLeft: 10}}>
+          ***
+      </Text>
+    </View>
+
+
+  );
+}
+
+
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* step03 - 'react-navigation' - [App.js]
-          - Incluir <NavigationContainer> e <Stack.Navigator> 
-          - e colocar as telas para navegação dentro delas */}
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          {/* /* step03 - 'react-navigation' - [App.js]
-            - Como especificar uma rota? usando o componente Screen
-            que recebe como propriedades name e component
-            - Aqui, usei a navegação por pilha (Stack) */}
-          <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Header sem customização: */}
+        {/* <Stack.Navigator initialRouteName="Home"> */}
+
+        {/* Header com customização: */}
+        {/* MODE 2 - 'header-customization - [App.js] */}
+        {/* customizando o header de todas as páginas em conjuntos*/}
+        {/* -> Personalização no Stack.Navigator -> "screenOptions" */}
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "rosybrown",
+            },
+            headerTintColor: "black",
+            headerTitleStyle: {
+              fontSize: 18,
+            },
+          }}>
+
+          {/* Header da Home sem customização: */}
+          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+
+          {/* Header com customização: */}
+          {/* MODE 1 - 'header-customization - [App.js] */}
+          {/* customizando o header de uma página em específico */}
+          {/* -> Personalização no Stack.Screen da tela desejada -> "options"*/}
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerStyle: {
+                backgroundColor: "silver",
+              },
+              headerTintColor: "purple",
+              headerTitleStyle: {
+                fontSize: 18,
+              },
+              headerTitleAlign: "center"
+            }} />
+
           <Stack.Screen name="Horoscope" component={HoroscopeScreen} options={{ title: 'My Horoscope' }} />
           <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: 'My Profile' }} />
-          <Stack.Screen name="Menu" component={MenuScreen} options={{ title: 'Menu' }} />
-          {/* step04 - 'react-navigation' - [App.js]
-              - Extras:
-                  - definir initialRouteName no 'Navigator'
-                  - add prop 'options' no componente 'Screen' -> para opções específicas para certa(s) tela 
-                  - add prop 'screenOptions' no componente 'Navigator' -> para opções gerais em todas telas.*/}
+
+          {/* <Stack.Screen name="Menu" component={MenuScreen} options={{ title: 'Menu' }} /> */}
+
+          {/* Header com customização: */}
+          {/* MODE 3B - 'header-customization - [App.js] */}
+          {/* Replacing the title with a custom component​ */}
+          {/* -> renderizar numa funcao fora, e chamar depois*/}
+          <Stack.Screen
+            name="Menu"
+            component={MenuScreen}
+            options={{
+              headerTitle: (props) => <MyCustomHeaderView {...props} />,
+              headerStyle: {
+                backgroundColor: "darksalmon",
+              },
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </QueryClientProvider>
